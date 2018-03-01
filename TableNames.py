7fc50e3945f0a76936d251bb7b01bb4d0c3a3a56
Greg-B <35638671+Greg-Bernard@users.noreply.github.com>
2018-03-01 12:38:22 -0500

    Updated for REST functionality

--- TableNames.py
+++ TableNames.py
@@ -7,3 +7,39 @@ DO NOT MODIFY
 
 tables = ['accounts', 'contacts', 'EmailOpen', 'EmailClickthrough', 'EmailSend', 'Subscribe', 'Unsubscribe',
           'Bounceback', 'WebVisit', 'PageView', 'FormSubmit']
+
+campaign_col_def = {
+            'currentStatus': 'TEXT',
+            'id': 'INTEGER PRIMARY KEY',
+            'createdAt': 'DATETIME',
+            'createdBy': 'INTEGER',
+            'depth': 'TEXT',
+            'name': 'TEXT',
+            'updatedAt': 'DATETIME',
+            'updatedBy': 'INTEGER',
+            'actualCost': 'REAL',
+            'budgetedCost': 'REAL',
+            'product': 'TEXT',
+            'region': 'TEXT',
+            'campaignCategory': 'TEXT',
+            'Field 1': 'TEXT',
+            'Field 2': 'TEXT',
+            'Field 3': 'TEXT',
+            'firstActivation': 'DATETIME',
+            'memberCount': 'INTEGER',
+            'startAt': 'DATETIME',
+            'endAt': 'DATETIME'
+        }
+
+external_col_def = {
+            'type': 'TEXT',
+            'id': 'INTEGER PRIMARY KEY',
+            'depth': 'TEXT',
+            'name': 'TEXT',
+            'activityDate': 'DATETIME',
+            'activityType': 'TEXT',
+            'assetName': 'TEXT',
+            'assetType': 'TEXT',
+            'campaignId': 'INTEGER',
+            'contactId': 'INTEGER'
+        }
